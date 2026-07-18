4093cbf13e37adc7a6bb98768469f69569329110
GodPedin <pedinbr4@gmail.com>
2026-07-18 11:23:11 -0300

    Adiciona script de instalação dos dotfiles

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+echo "Instalando dotfiles..."
+
+# Zsh
+cp zsh/.zshrc ~/
+[ -f zsh/.p10k.zsh ] && cp zsh/.p10k.zsh ~/
+
+# Fastfetch
+mkdir -p ~/.config/fastfetch
+cp -r fastfetch/* ~/.config/fastfetch/
+
+# KDE
+cp kde/kdeglobals ~/.config/
+cp kde/kwinrc ~/.config/
+cp kde/plasmarc ~/.config/
+cp kde/plasma-org.kde.plasma.desktop-appletsrc ~/.config/
+cp kde/kscreenlockerrc ~/.config/
+[ -f kde/ksplashrc ] && cp kde/ksplashrc ~/.config/
+
+echo "Concluido.Reinicie a sessao"
